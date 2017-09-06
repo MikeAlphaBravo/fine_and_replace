6121372b04271eb6798ac306d436b1a6f42c739f
Michael Brooks and Noah Gottlieb <michael@>
2017-09-06 13:32:07 -0700

    complete testing for working code and spec.  It will also use a shorter syntax to achieve results.

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,1 +1,28 @@
 #!/usr/bin/env ruby
+class Replace
+def initialize(sentence)
+  @sentence = sentence
+end
+
+  def search(word1,word2)
+    user_string = @sentence
+    if user_string.include? word1
+      user_string.gsub(word1, word2)
+    else
+      user_string
+    end
+ end
+end
+
+
+# input = "hello world".split(//)
+# x=0
+# while(x<input[x].length)
+#   if(input[x] === "h")
+#     input[x] === "w"
+#     input.push("w")
+#   break
+# end
+# end
+# print input
+# end
--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -1,9 +1,10 @@
 require "rspec"
 require "pry"
-require "fine_and_replace"
+require "find_and_replace"
 
-describe ("#fine_and_replace") do
-  it ("") do
-    expect ()
+describe("#find_and_replace") do
+  it("take a word and replace it with another") do
+    words = Replace.new("work with this")
+    expect(words.search("work","play")).to(eq("play with this"))
   end
 end
